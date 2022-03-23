d60e8e41a86ede9d5ffb84eae21be1894cab3bf1
antcho1024 <50614113+antcho1024@users.noreply.github.com>
2022-03-23 11:41:36 +0900

    Airplane 정해진 경로 움직이기

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -2,29 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//실슴 
-//비행기 게임 오브젝트 만들기(프리펩)
-//이름 : Airplane
-//방식 : 프리미티브 사용 or 프로빌더
-//true면 프로펠러 돌아가고 fasle면 안돌아감 
 public class Airplane : MonoBehaviour
 {
     public bool propellerOn = false; // true면 프로펠러 돌아가고 fasle면 안돌아감
     //private GameObject propObj = null;
     private Transform propTransform = null;
     private float propSpeed = 720.0f;
+    public float moveSpeed = 3.0f;
+    public Transform[] waypoints = null;
+
+    private int waypointIndex = 0;
 
     private void Awake()
     {
         //propObj = transform.Find("Propeller").gameObject;
         propTransform = transform.Find("Propeller"); //transform 자식 중에서 이름이 프로펠러 인 트랜스폼 찾ㄱ
     }
+    private void Start()
+    {
+        propellerOn = true;
+        if (waypoints.Length > 0)
+        {
+            waypointIndex = 0;
+            transform.LookAt(waypoints[waypointIndex]);
+        }
+        else
+            Debug.Log("waypoint가 없음");
+    }
     private void Update()
     {
-        if(propellerOn)
+        //transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);        
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime, Space.World);
+        if (CheckArrive())
+        {
+            GoNextWaypoint();
+        }
+        if (propellerOn)
         {
             propTransform.Rotate(0, propSpeed * Time.deltaTime, 0);// 초당 두바퀴
         }
+
+    }
+
+    private bool CheckArrive()
+    {
+        //waypoints[waypointIndex].position; 도착 지점
+        //transform.position 출발 지점
+        Vector3 distance = waypoints[waypointIndex].position - transform.position; //도착지점 - 출발 지
+        return distance.sqrMagnitude < 0.1f;
+    }
+
+    void GoNextWaypoint()
+    {
+        waypointIndex++;
+        waypointIndex %= waypoints.Length;
+        transform.LookAt(waypoints[waypointIndex]);
     }
 
 
